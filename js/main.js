a0e03d07041496bdf516c1e588b0b73dc74e62ae
Chen-H115 <Chen-H115@github.com>
2018-12-30 23:37:07 +0800

    test1.7

---
+++ js/main.js
@@ -0,0 +1,30 @@
+;$(function()
+{
+  'use strict';
+
+  var sidebar = $('#sidebar'),
+      mask = $('.mask'),
+      sidebar_trigger = $('#sidebar_trigger'),
+      condition = 0;
+
+  sidebar_trigger.on('click', function()
+  {
+    if (condition % 2 == 0) {
+      mask.fadeIn();
+      sidebar.css('right', 0);
+      condition = condition + 1;
+    }
+    else {
+      mask.fadeOut();
+      sidebar.css('right', -sidebar.width());
+      condition = condition + 1;
+    }
+  })
+  mask.on('click', function()
+  {
+    mask.fadeOut();
+    sidebar.css('right', -sidebar.width());
+    condition = condition + 1;
+  }
+  )
+})
